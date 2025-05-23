3ef446b893773037bc5f04cfff52d93008c0cbc2
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-05-23 15:00:46 +0800

    psgl triangle demo

--- psgl/main.cpp
+++ psgl/main.cpp
@@ -1,10 +1,4 @@
-/* SCE CONFIDENTIAL
- * PlayStation(R)3 Programmer Tool Runtime Library 475.001
- *                Copyright (C) 2010 Sony Computer Entertainment Inc.
- *                                               All Rights Reserved.
- */
 #include <math.h>
-#include <cell/fs/cell_fs_file_api.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -12,108 +6,251 @@
 #include <sys/process.h>
 #include <sys/spu_initialize.h>
 #include <sys/paths.h>
+#include <sys/sys_time.h>
 
 #include <PSGL/psgl.h>
 #include <PSGL/psglu.h>
 #include <Cg/cg.h>
 
 #include <cell/dbgfont.h>
+#include <cell/fs/cell_fs_file_api.h>
+#include <cell/sysmodule.h>
 
-#include "Common/gfxCommon.h"
-#include "Common/gfxObject.h"
 
-#include <sys/sys_time.h>
-
-#include "cube.h"		// our test data
-
-using namespace Vectormath;
-using namespace Aos;
-
-#define SAMPLE_NAME "Basic Cg Example"
-
-// switch to true to run-time compile the shaders 
-const bool LOAD_SHADER_BINARY = true; 
 const bool VSYNC_ON = false; 
 
+GLuint 		gfxWidth = 1280;
+GLuint 		gfxHeight = 720;
+GLfloat   gfxAspectRatio = 16.0f/9.0f;
+
 // shader binary 
-#define VERTEX_PROGRAM_BINARY		  SYS_APP_HOME"/shaders/vs_basicSample.cgelf"
-#define FRAGMENT_PROGRAM_BINARY		SYS_APP_HOME"/shaders/fs_basicSample.cgelf"
+#define VERTEX_PROGRAM_BINARY		  SYS_APP_HOME"/shaders/triangle_fs.cgelf"
+#define FRAGMENT_PROGRAM_BINARY		SYS_APP_HOME"/shaders/triangle_vs.cgelf"
 
-// shader sources 
-#define VERTEX_PROGRAM_SOURCE		  SYS_APP_HOME"/shaders/vs_basicSample.cg"
-#define FRAGMENT_PROGRAM_SOURCE		SYS_APP_HOME"/shaders/fs_basicSample.cg"
-
-
-gfxObject Object;			//a simple object to render
 
 CGprogram 		VertexProgram;			//loaded vertex program
-CGprogram 		FragmentProgram;		//loaded fragment program
-	
-CGparameter  		ModelViewProj_cgParam;		//uniform parameter in vertex program
-CGparameter		DiffuseMap_cgParam;		//uniform parameter in fragment program
-
-Vector3       		ViewPos;    
-Vector3       		ViewAng;
-Matrix4       		View;									//current view ( camera ) matrix
-
-//-----------------------------------------------------------------------------
-// DEFINES
-//-----------------------------------------------------------------------------
-#define CAM_SPEED 0.5f
-#define CAM_RPY_SPEED 1.0f
-#define DEFAULT_VIEW_DIST  -5.f		// setting up the view
-
-#define NEAR_CLIP	0.1f
-#define FAR_CLIP	8500.1f
-#define FOV_Y		40.0f			//field of view in the y direction
+CGprogram 		FragmentProgram;		//loaded fragment program  
 
 
-// FPS REPORTING
-float 			FPS = 100.0f; 
-float			LastTime = 0;
-const static float 	REPORT_TIME = 2.0f;  
-int 			Frames = 0;
-int 			FramesLastReport = 0;
-double 			TimeElapsed = 0;
-double 			TimeLastReport = 0;
-double 			TimeReport = 0;
-unsigned int 		Counter = 0;		//frame counter
+float vertices[] = {
+  -0.5f, -0.5f, 0.0f,
+   0.5f, -0.5f, 0.0f,
+   0.0f,  0.5f, 0.0f,
+};
 
+float colors[] = {
+  1.0f, 0.0f, 0.0f,
+  0.0f, 1.0f, 0.0f,
+  0.0f, 0.0f, 1.0f,
+};
 
+GLuint PositionVBO;
+GLuint ColorVBO;
+const int VerticesNum = 3;
+
+CGprogram gfxLoadProgramFromFile(CGprofile target, const char* filename)
+{
+  CGprogram id = cgCreateProgramFromFile(cgCreateContext(), CG_BINARY, filename, target, NULL, NULL);
+  if(!id)
+  {
+    printf("Failed to load shader program >>%s<<\nExiting\n", filename);
+    exit(0);
+  }
+  else
+  return id;
+}
+
+static void gfxSysutilCallback(uint64_t status, uint64_t param, void* userdata)
+{
+	(void) param;
+	(void) userdata;
+
+	switch(status) {
+	case CELL_SYSUTIL_REQUEST_EXITGAME:
+		glFinish();
+	  exit(0);
+		break;
+	case CELL_SYSUTIL_DRAWING_BEGIN:
+	case CELL_SYSUTIL_DRAWING_END:
+		break;
+	default:
+		printf( "Graphics common: Unknown status received: 0x%llx\n", status );
+	}
+}
+
+void gfxRegisterSysutilCallback()
+{
+	// Register sysutil exit callback
+	int ret = cellSysutilRegisterCallback(0, gfxSysutilCallback, NULL);
+	if( ret != CELL_OK ) {
+		printf( "Registering sysutil callback failed...: error=0x%x\n", ret );
+		exit(1);
+	}
+}
+
+void gfxInitGraphics() 
+{
+  // Load required prx modules.
+  int ret = cellSysmoduleLoadModule(CELL_SYSMODULE_GCM_SYS);
+  switch( ret )
+  {
+      case CELL_OK:
+        // The module is successfully loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_DUPLICATED:
+        // The module was already loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_UNKNOWN:
+      case CELL_SYSMODULE_ERROR_FATAL:
+    printf("!! Failed to load CELL_SYSMODULE_GCM_SYS\n" ); 
+    printf("!! Exiting Program \n" ); 
+    exit(1);
+  }
+
+  
+  ret = cellSysmoduleLoadModule(CELL_SYSMODULE_FS);
+  switch( ret )
+  {
+      case CELL_OK:
+        // The module is successfully loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_DUPLICATED:
+        // The module was already loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_UNKNOWN:
+      case CELL_SYSMODULE_ERROR_FATAL:
+    printf("!! Failed to load CELL_SYSMODULE_FS\n" ); 
+    printf("!! Exiting Program \n" ); 
+    exit(1);
+  }
+
+  ret = cellSysmoduleLoadModule(CELL_SYSMODULE_USBD);
+  switch( ret )
+  {
+      case CELL_OK:
+        // The module is successfully loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_DUPLICATED:
+        // The module was already loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_UNKNOWN:
+      case CELL_SYSMODULE_ERROR_FATAL:
+    printf("!! Failed to load CELL_SYSMODULE_USBD\n" ); 
+    printf("!! Exiting Program \n" ); 
+    exit(1);
+  }
+
+  ret = cellSysmoduleLoadModule(CELL_SYSMODULE_IO);
+  switch( ret )
+  {
+      case CELL_OK:
+        // The module is successfully loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_DUPLICATED:
+        // The module was already loaded,
+    break;
+
+      case CELL_SYSMODULE_ERROR_UNKNOWN:
+      case CELL_SYSMODULE_ERROR_FATAL:
+    printf("!! Failed to load CELL_SYSMODULE_IO\n" ); 
+    printf("!! Exiting Program \n" ); 
+    exit(1);
+  }
+
+  gfxRegisterSysutilCallback();
+  
+  // First, initialize PSGL
+  // Note that since we initialized the SPUs ourselves earlier we should
+  // make sure that PSGL doesn't try to do so as well.
+  PSGLinitOptions initOpts={
+        enable: PSGL_INIT_MAX_SPUS | PSGL_INIT_INITIALIZE_SPUS | PSGL_INIT_HOST_MEMORY_SIZE,
+    maxSPUs: 1,
+    initializeSPUs: false,
+    // We're not specifying values for these options, the code is only here
+    // to alleviate compiler warnings.
+    persistentMemorySize: 0,
+    transientMemorySize: 0,
+    errorConsole: 0,
+    fifoSize: 0,	
+    hostMemorySize: 128* 1024*1024,  // 128 mbs for host memory 
+  };
+
+  psglInit(&initOpts);
+
+  static PSGLdevice* device=NULL;
+  //device=psglCreateDeviceAuto(GL_ARGB_SCE,GL_DEPTH_COMPONENT24,GL_MULTISAMPLING_NONE_SCE);
+  //device=psglCreateDeviceAuto(GL_ARGB_SCE,GL_DEPTH_COMPONENT24,GL_MULTISAMPLING_2X_DIAGONAL_CENTERED_SCE);
+  device=psglCreateDeviceAuto(GL_ARGB_SCE,GL_DEPTH_COMPONENT24,GL_MULTISAMPLING_4X_SQUARE_ROTATED_SCE);
+  
+  if ( !device )
+  {
+    printf("!! Failed to init the device \n" ); 
+    printf("!! Exiting Program \n" ); 
+    exit(1); 
+  }
+  psglGetDeviceDimensions(device,&gfxWidth,&gfxHeight);
+
+  printf("gfxInitGraphics::PSGL Device Initialized Width %d Height %d \n",gfxWidth, gfxHeight ); 	
+
+  gfxAspectRatio = psglGetDeviceAspectRatio(device);
+
+  // Now create a PSGL context
+  PSGLcontext *pContext=psglCreateContext();
+
+  if (pContext==NULL) {
+    fprintf(stderr, "Error creating PSGL context\n");
+    exit(-1);
+  }
+
+  // Make this context current for the device we initialized
+  psglMakeCurrent(pContext, device);
+
+  // Since we're using fixed function stuff (i.e. not using our own shader
+  // yet), we need to load shaders.bin that contains the fixed function 
+  // shaders.
+  // psglLoadShaderLibrary( REMOTE_PATH"/shaders.bin");
+  psglLoadShaderLibrary( SYS_APP_HOME"/shaders/shaders.bin");
+
+  // Reset the context
+  psglResetCurrentContext();
+  
+  glViewport(0, 0, gfxWidth, gfxHeight);
+  glScissor(0, 0, gfxWidth, gfxHeight);
+  glClearColor(0.f, 0.f, 0.f, 1.f);
+  glEnable(GL_DEPTH_TEST);
+
+  // PSGL doesn't clear the screen on startup, so let's do that here.
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+  psglSwap();
+}
 
 void sampleInit() 
 {
-
-  Counter=0;
-  // FPS REPORTING
-  Frames=0;
-  FramesLastReport=0;
-  TimeElapsed=0.0;
-  TimeLastReport=0.0;
-  TimeReport=REPORT_TIME;
-
-  glClearColor(0.3f,0.3f,0.7f, 0.0f);
+  glClearColor(0.1f,0.3f,0.3f, 1.0f);
   glClearDepthf(1.0f);
   glEnable(GL_DEPTH_TEST);
-  glEnable (GL_CULL_FACE);
+  glEnable(GL_CULL_FACE);
 
-  //initialize the object
-  Object.setMesh(CUBEVCOUNT,cubeST,cubeNorm,cubeVert);
-  Object.mTexture = gfxCreateCheckerTexture(8,0x000000FF,0xFF0000FF);
-  Object.mRotD = Vector3(0.003f,0.004f,0.002f);    //this simply makes the object spin
+  glGenBuffers(1,&PositionVBO);
+  glBindBuffer(GL_ARRAY_BUFFER,PositionVBO);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(GLfloat)*3*VerticesNum,vertices,GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  if ( LOAD_SHADER_BINARY )
-  {
-  	// load the shader programs
-	VertexProgram      = gfxLoadProgramFromFile(cgGLGetLatestProfile(CG_GL_VERTEX), VERTEX_PROGRAM_BINARY);
-	FragmentProgram    = gfxLoadProgramFromFile(cgGLGetLatestProfile(CG_GL_FRAGMENT), FRAGMENT_PROGRAM_BINARY);
-  }
-  else
-  {
-  	// load the shader programs
-	VertexProgram      = gfxLoadProgramFromSource(cgGLGetLatestProfile(CG_GL_VERTEX), VERTEX_PROGRAM_SOURCE);
-	FragmentProgram    = gfxLoadProgramFromSource(cgGLGetLatestProfile(CG_GL_FRAGMENT), FRAGMENT_PROGRAM_SOURCE);
-  }
+  glGenBuffers(1,&ColorVBO);
+  glBindBuffer(GL_ARRAY_BUFFER,ColorVBO);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(GLfloat)*3*VerticesNum,colors,GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // load the shader programs
+  VertexProgram      = gfxLoadProgramFromFile(cgGLGetLatestProfile(CG_GL_VERTEX), VERTEX_PROGRAM_BINARY);
+  FragmentProgram    = gfxLoadProgramFromFile(cgGLGetLatestProfile(CG_GL_FRAGMENT), FRAGMENT_PROGRAM_BINARY);
 
   // bind and enable the vertex and fragment programs
   cgGLBindProgram(VertexProgram);
@@ -121,157 +258,55 @@ void sampleInit()
   cgGLEnableProfile(cgGLGetLatestProfile(CG_GL_VERTEX));
   cgGLEnableProfile(cgGLGetLatestProfile(CG_GL_FRAGMENT));
 
-  //get handles to the uniform cg parameters that need setting
-  ModelViewProj_cgParam = cgGetNamedParameter(VertexProgram, "modelViewProj");      gfxCheckCgError (__LINE__);
-  DiffuseMap_cgParam = cgGetNamedParameter (FragmentProgram, "diffuseMap");         gfxCheckCgError (__LINE__);
-
-  //set the viewing parameters
-  ViewPos = Vector3(0.0f,0.0f,DEFAULT_VIEW_DIST);
-  ViewAng = Vector3(0.0f,0.0f,0.0f);
-
   //FPS reporting (disable vsync to get true time)
-  glDisable(GL_VSYNC_SCE);
-  
-  // last the projection matrix 
-  glMatrixMode( GL_PROJECTION );
-  glLoadIdentity();
-  gluPerspectivef(FOV_Y, gfxGetAspectRatio(), NEAR_CLIP,FAR_CLIP);
-  glMatrixMode( GL_MODELVIEW );
-
-}
-//-----------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------
-// void sampleUpdateMatrices()
-// Description: 
-// Returns: NA
-// Notes:
-//-----------------------------------------------------------------------------
-void sampleUpdateMatrices()
-{
-  Object.update();		//updates the objects orientation and local to world matrix
-
-  // update the view matrix
-  View   = Matrix4::identity ();
-  Matrix4 trans   = Matrix4::translation (ViewPos);	 // set the translation
-  Matrix4 rotX    = Matrix4::rotationX (ViewAng[0]);	 // set an x rotation  
-  Matrix4 rotY    = Matrix4::rotationY (ViewAng[1]);	 // set a y roation 
-  View =   trans * rotX * rotY ;	//multiply them all together
-
-  Counter++;
-
-}
-//-----------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------
-// void sampleRender()
-// Description: 
-// Returns: NA
-// Notes:
-//-----------------------------------------------------------------------------
-void sampleRender()
-{
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-
-  //set the modelview matrix to contain the view matrix
-  glPushMatrix();
-  glLoadIdentity();
-  glLoadMatrixf((GLfloat*)&View);                            
-
-  glPushMatrix();
-
-  //apply the local to world matrix to get the complete  model to view matrix set as current
-  glMultMatrixf((GLfloat*)&Object.mLocalToWorld);		//load the local to world matrix
-
-
-  cgGLEnableProfile(cgGLGetLatestProfile(CG_GL_VERTEX));	//enable the shaders
-  cgGLEnableProfile(cgGLGetLatestProfile(CG_GL_FRAGMENT));
-  
-  //set the uniforma paramters in the cg programs
-  cgGLSetStateMatrixParameter (ModelViewProj_cgParam, CG_GL_MODELVIEW_PROJECTION_MATRIX, CG_GL_MATRIX_IDENTITY); gfxCheckCgError (__LINE__);
-  cgGLSetTextureParameter (DiffuseMap_cgParam, Object.mTexture);   gfxCheckCgError (__LINE__);
-  cgGLEnableTextureParameter (DiffuseMap_cgParam);                 gfxCheckCgError (__LINE__);
-
-  Object.render();		//draws the object
-
-  cgGLDisableTextureParameter (DiffuseMap_cgParam);                gfxCheckCgError (__LINE__);
-  cgGLDisableProfile(cgGLGetLatestProfile(CG_GL_VERTEX));	//disable the shaders
-  cgGLDisableProfile(cgGLGetLatestProfile(CG_GL_FRAGMENT));
-
-  glPopMatrix();
-  glPopMatrix();
-
-
-  glDisable(GL_BLEND);
-  glEnable(GL_DEPTH_TEST);
-
-}
-
-void sampleUpdateFPS()
-{
-
-
-  // FPS REPORTING
-  // get current timing info
-  float timeNow = (float)sys_time_get_system_time() * .000001f;
-  float fElapsedInFrame = (float)(timeNow - LastTime);
-  LastTime = timeNow;
-  ++Frames;
-  TimeElapsed+=fElapsedInFrame;
-  // report fps at appropriate interval
-  if (TimeElapsed>=TimeReport)
-  {
-  	FPS = (Frames-FramesLastReport)*1.f/(float)(TimeElapsed-TimeLastReport);
-	printf("FPS: %.2f\n",FPS);
-	TimeReport+=REPORT_TIME;
-	TimeLastReport=TimeElapsed;
-	FramesLastReport=Frames;
-  }
-
-  dbgFontPrintf(40,60,0.5f,"%s %.5f FPS", SAMPLE_NAME, FPS );
-  if( VSYNC_ON )
-  	dbgFontPrintf(40,70,0.5f,"VSYNC ON" );
+  if (VSYNC_ON)
+    glEnable(GL_VSYNC_SCE);
   else
-  	dbgFontPrintf(40,70,0.5f,"VSYNC OFF" );
-  	
-  dbgFontDraw();
-
+    glDisable(GL_VSYNC_SCE);
 }
+//-----------------------------------------------------------------------------
+
 
 SYS_PROCESS_PARAM(1001, 0x10000)
 
 int main()
 {
+  // Initialize 6 SPUs but reserve 1 SPU as a raw SPU for PSGL
+  sys_spu_initialize(6, 1);	
+  
+  // init PSGL and get the current system width and height
+  gfxInitGraphics();	
+  
+  // initialize sample data and projection matrix 
+  sampleInit();
+  
+  while (1)
+  {
+    // PSGL doesn't clear the screen on startup, so let's do that here.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);		
 
-	// Initialize 6 SPUs but reserve 1 SPU as a raw SPU for PSGL
-	sys_spu_initialize(6, 1);	
-	
-	// init PSGL and get the current system width and height
-	gfxInitGraphics();
+    // render 
+    {
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glBindBuffer(GL_ARRAY_BUFFER, PositionVBO);
+      glVertexPointer(3,GL_FLOAT,0,NULL);
 
-	// initalize the dbgFonts 
-	dbgFontInit();		
-	
-	// initialize sample data and projection matrix 
-	sampleInit();
-	
-	while (1)
-	{
-		// PSGL doesn't clear the screen on startup, so let's do that here.
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);		
+      glEnableClientState(GL_COLOR_ARRAY);
+      glBindBuffer(GL_ARRAY_BUFFER, ColorVBO);
+      glColorPointer(3,GL_FLOAT,0,NULL);
 
-		// update the matrices 
-		sampleUpdateMatrices();
-		// render 
-		sampleRender(); 
-		// update FPS
-		sampleUpdateFPS(); 
-				
-		// swap PSGL buffers 
-		psglSwap();
-	}
+      glDrawArrays(GL_TRIANGLES,0,VerticesNum);
+      
+      glBindBuffer(GL_ARRAY_BUFFER, 0 );	 	
+      glDisableClientState(GL_COLOR_ARRAY);
+      glDisableClientState(GL_VERTEX_ARRAY);
+
+      // glDisable(GL_BLEND);
+      // glEnable(GL_DEPTH_TEST);
+    }
+        
+    // swap PSGL buffers 
+    psglSwap();
+  }
 }
 
